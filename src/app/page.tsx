93b14dcf77054ff3ae38d39e52b5e54a55529dbf
Ifadha <198208527+Ifadha2004@users.noreply.github.com>
2026-05-31 17:59:08 +0400

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import Navbar from "../components/layout/navbar";
 import Footer from "../components/layout/footer";
 import Hero from "../components/sections/hero-section";
-// import InteriorSolutionsBanner from "../components/sections/InteriorSolutionsBanner";
-// import SolutionsGridSection from "../components/sections/solutions-section";
 import InteriorSolutionsUnified from '../components/sections/InteriorSolutionsUnified';
-// import VideoShowcase from "../components/sections/VideoShowcase";
-// import WhyChooseUs from "../components/sections/why-choose-section";
 import WhyIdealFactory from "../components/sections/WhyIdealFactory";
 import GallerySection from "../components/sections/gallery";
 import FeatureBanner from "../components/sections/feature-banner";
@@ -19,11 +15,7 @@ export default function HomePage() {
 
       <main>
         <Hero />
-        {/* <InteriorSolutionsBanner />
-        <SolutionsGridSection /> */}
         <InteriorSolutionsUnified />
-        {/* <VideoShowcase /> */}
-        {/* <WhyChooseUs /> */}
         <WhyIdealFactory />
         <GallerySection />
         <FeatureBanner />
